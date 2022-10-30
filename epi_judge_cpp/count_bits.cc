9b5b13cbd44a3c7be92f6dd6cd1c856d4c2809a4
Mohammed Ashiqur Rahman Baig <ashiqur1107@gmail.com>
2022-10-30 21:32:32 +0600

    OCT 30 - 2022 count_bits.cc Solved parity.cc Solved

--- epi_judge_cpp/count_bits.cc
+++ epi_judge_cpp/count_bits.cc
@@ -1,11 +1,18 @@
 #include "test_framework/generic_test.h"
 
-short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+short CountBits(unsigned int x)
+{
+  short result = 0;
+  while (x)
+  {
+    result += (x & 1);
+    x >>= 1;
+  }
+  return result;
 }
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
   return GenericTestMain(args, "count_bits.cc", "count_bits.tsv", &CountBits,
